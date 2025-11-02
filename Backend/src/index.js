f3a70105907f78b89e9f227f4c88ba2302270ace
gaurav0973 <codejourneypanda@gmail.com>
2025-11-02 16:51:41 +0530

    fix bug

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -20,7 +20,7 @@ const app = express()
 
 // global pody-parser
 app.use(cors({
-    origin : "http://localhost:5173",
+    origin : process.env.FRONTEND_URL,
     credentials : true
 }))
 app.use(express.json())
