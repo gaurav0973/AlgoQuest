f9a114943dd04997f04af42e91eaea42de564a2e
gaurav0973 <codejourneypanda@gmail.com>
2025-11-03 20:13:54 +0530

    fix bug : proxy pass

--- Frontend/src/utils/constant.js
+++ Frontend/src/utils/constant.js
@@ -1,4 +1,4 @@
 export const API_BASE_URL = 
   location.hostname === "localhost" || location.hostname === "127.0.0.1"
     ? "http://localhost:8888"   
-    : "";                       
+    : "/api";                       
