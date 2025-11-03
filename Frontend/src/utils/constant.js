5d75026382ed9d16f1f5baf9238074aa049fdc61
gaurav0973 <codejourneypanda@gmail.com>
2025-11-03 20:36:11 +0530

    bug

--- Frontend/src/utils/constant.js
+++ Frontend/src/utils/constant.js
@@ -1,4 +1,3 @@
-export const API_BASE_URL =
-  location.hostname === "localhost" || location.hostname === "127.0.0.1"
-    ? "http://localhost:8888"
-    : "/api"; // This will now properly route through nginx
+export const API_BASE_URL = location.hostname === "localhost" 
+  ? "http://localhost:8888" 
+  : "/api";
