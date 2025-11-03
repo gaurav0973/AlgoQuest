3fdfeca02b5212da0fa5e933992bd2e1a415d1c9
gaurav0973 <codejourneypanda@gmail.com>
2025-11-04 02:51:27 +0530

    container backend

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -16,13 +16,11 @@ const app = express();
 app.use(
   cors({
     origin: [
+      "http://localhost:5173",
+      "http://localhost:5174",
       process.env.FRONTEND_URL,
-      "http://localhost:5173"
     ],
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization", "Cookie"],
-    exposedHeaders: ["set-cookie"],
   })
 );
 app.use(express.json());
@@ -34,10 +32,10 @@ app.use("/problem", problemRouter);
 app.use("/submission", submitRoute);
 app.use("/video", videoRoute);
 
-const PORT = 8888 | process.env.PORT;
+const PORT =  process.env.PORT || 4000
 connectDB()
   .then(async () => {
-    app.listen(PORT, () => {
+    app.listen(PORT, "0.0.0.0", () => {
       console.log(`Server is running at : ${PORT}`);
     });
   })
--- Frontend/src/utils/constant.js
+++ Frontend/src/utils/constant.js
@@ -1,3 +1,3 @@
 export const API_BASE_URL = location.hostname === "localhost" 
-  ? "http://localhost:8888" 
-  : "/api";
+  ? "http://localhost:4000" 
+  : "";
